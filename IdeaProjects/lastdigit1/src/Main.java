e83d02b4114214cf2e345197113f40004ff83db1
nataniyasu-hash <nataniyasu@gmail.com>
2025-11-27 23:08:44 +0100

    lastdigit

---
+++ IdeaProjects/lastdigit1/src/Main.java
@@ -0,0 +1,30 @@
+/**
+ * ## Letzte Ziffer
+ *
+ * Entwickeln Sie nun bitte eine Methode `lastDigit()`, die
+ * für zwei Zahlen (Dezimalnotation) prüft, ob diese dieselbe letzte Ziffer haben.
+ *
+ * Aufruf Beispiele finden Sie in der `main()`-Methode.
+ *
+ * __Hinweise:__
+ *
+ * - Der Modulo Operator % ist sicher hilfreich (siehe Unit 03, arithmetische Operatoren)
+ * - Einen Betrag können Sie mittels `Math.abs()` bestimmen.
+ *
+ */
+class Main {
+
+    public static boolean lastDigit(int a, int b) {
+        int letzteZifferA = a % 10;
+        int letzteZifferB = b % 10;
+        if (letzteZifferB == letzteZifferA) {
+            return true;
+        }
+        return false;
+    }
+    public static void main(String[] args) {
+        boolean result = lastDigit(21, 12);
+        System.out.println(result); // => false
+        System.out.println(lastDigit(121, 2001)); // => true
+    }
+}
