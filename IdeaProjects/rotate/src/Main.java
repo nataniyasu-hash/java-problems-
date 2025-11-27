8ded959da6d813790f0f55c53b8b2e8950b18d2b
nataniyasu-hash <nataniyasu@gmail.com>
2025-11-27 23:06:19 +0100

    rotate

---
+++ IdeaProjects/rotate/src/Main.java
@@ -0,0 +1,41 @@
+/**
+ * ## Zeichenketten "rotieren"
+ *
+ * Entwickeln Sie nun bitte eine Methode `rotate()`, die eine Zeichenkette
+ * nach links oder rechts "rotiert". Zeichen die links oder rechts aus
+ * der Zeichenkette "geschoben" werden, sollen rechts bzw. links wieder
+ * "hineingeschoben" werden.
+ *
+ * Aufrufbeispiele finden Sie in der `main()`-Methode.
+ *
+ * __Hinweise:__
+ *
+ * - Beachten Sie, dass eine Rotation positiv und negativ sein kann.
+ * - Beachten Sie, dass eine Rotation länger als die eigentliche Zeichenkette sein kann.
+ * - Das Problem lässt sich tatsächlich ohne Schleife lösen (sicherlich aber auch mit ;-).
+ *
+ */
+class Main {
+
+    public static String rotate(int n, String s) {
+
+        int length = s.length();
+        n %= length;
+        if(n == 0) {
+            return s;
+        }
+        if (n < 0) {
+            n = n + length;
+        }
+        String beginn = s.substring(length -n);
+        String ende = s.substring(0, length - n);
+return beginn + ende;
+    }
+
+    public static void main(String[] args) {
+        String result = rotate(2, "Hello");
+        System.out.println(result); // => "loHel"
+        System.out.println(rotate(3, "Hello")); // => "lloHe"
+        System.out.println(rotate(6, "Hello")); // => "oHell"
+    }
+}
