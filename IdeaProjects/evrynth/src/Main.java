243e39b6cf4e83a08d2165387f6ab6a1e779c02a
nataniyasu-hash <nataniyasu@gmail.com>
2025-12-25 15:46:11 +0100

    luxkysum

--- IdeaProjects/evrynth/src/Main.java
+++ IdeaProjects/evrynth/src/Main.java
@@ -1,43 +1,36 @@
 /**
- * ## Aufsteigend sortierte Tripple finden
+ * ## Addieren mit der `luckySum()`
  *
- * Schreiben Sie nun bitte eine Methode `trippleUp()`
- * die in einem Integer-Array prüft, ob dieses
- * drei aufsteigende, benachbarte Werte wie bspw. 1, 2, 3,
- * oder auch 24, 25, 26 beinhaltet.
+ * Entwickeln Sie nun bitte eine Methode `luckySum()`, die eine
+ * variable Anzahl von ganzzahligen Parametern solange aufaddiert bis
+ * der Wert 13 in einem Parameter auftaucht.
  *
  * Aufrufbeispiele finden Sie in der `main()`-Methode.
  *
+ * __Hinweis:__
+ *
+ * - Sehen Sie sich noch einmal variable Parameter in Unit 2 (Methoden) an.
+ *
  */
 class Main {
-    public static boolean trippleUp(int[] array) {
-        int count = 1;
-        int erste = array[0];
-        for (int i = 1; i < array.length; i++) {
-            if(erste + 1 == array[i]) {
-                count++;
-                erste = array[i];
-            } else {
-                if (count >= 3) return true;
-                if(count < 3) {
-                    count = 1;
-                    erste = array[i];
-                }
+    public static int luckySum(int ... zahlen) {
+        int sum = 0;
+        for (int i = 0; i < zahlen.length; i++) {
+            if (zahlen[i] != 13) {
+                sum += zahlen[i];
+            }
+            if (zahlen[i] == 13) {
+                break;
             }
         }
-        if (count >= 3 ) return true;
-        return false;
+        return sum;
     }
 
     public static void main(String[] args) {
-        int[] a1 = {1, 4, 5, 6, 2};
-        int[] a2 = {1, 2, 3};
-        int[] a3 = {1, 2, 4};
-        int[] a4 = {3, 2, 1};
-
-        System.out.println(trippleUp(a1)); // => true
-        System.out.println(trippleUp(a2)); // => true
-        System.out.println(trippleUp(a3)); // => false
-        System.out.println(trippleUp(a4)); // => false
+        int result = luckySum(5, 6, 13, 8);
+        System.out.println(result); // => 11
+        System.out.println(luckySum(1, 2, 3, 4, 5)); // => 15
+        System.out.println(luckySum(1, 2)); // => 3
+        System.out.println(luckySum(13)); // => 0
     }
 }
