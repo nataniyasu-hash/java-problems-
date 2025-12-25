7b53ae8318c5a30e40a869615626b9e50b40bbae
nataniyasu-hash <nataniyasu@gmail.com>
2025-12-25 15:51:16 +0100

    scoresIncreasing revision

--- IdeaProjects/evrynth/src/Main.java
+++ IdeaProjects/evrynth/src/Main.java
@@ -1,36 +1,44 @@
 /**
- * ## Addieren mit der `luckySum()`
+ * ## Sortierung von Arrays prüfen
  *
- * Entwickeln Sie nun bitte eine Methode `luckySum()`, die eine
- * variable Anzahl von ganzzahligen Parametern solange aufaddiert bis
- * der Wert 13 in einem Parameter auftaucht.
+ * Sie sollen nun eine Methode `scoresIncreasing()` entwickeln,
+ * die für ein gegebenes Array prüft, ob alle Wert in diesem
+ * Array aufsteigend sortiert sind oder das Array leer ist.
  *
  * Aufrufbeispiele finden Sie in der `main()`-Methode.
  *
  * __Hinweis:__
  *
- * - Sehen Sie sich noch einmal variable Parameter in Unit 2 (Methoden) an.
+ * - Sehen Sie sich Arrays in Unit 03 noch einmal an.
  *
  */
 class Main {
-    public static int luckySum(int ... zahlen) {
-        int sum = 0;
-        for (int i = 0; i < zahlen.length; i++) {
-            if (zahlen[i] != 13) {
-                sum += zahlen[i];
-            }
-            if (zahlen[i] == 13) {
-                break;
-            }
+    public static boolean scoresIncreasing(int[] array) {
+        if (array.length == 0) return false;
+    int temp = array[0];
+
+    for (int i = 1; i < array.length; i++) {
+        if (temp > array[i]) {
+            return false;
         }
-        return sum;
+        temp = array[i];
+    }
+    return true;
     }
 
     public static void main(String[] args) {
-        int result = luckySum(5, 6, 13, 8);
-        System.out.println(result); // => 11
-        System.out.println(luckySum(1, 2, 3, 4, 5)); // => 15
-        System.out.println(luckySum(1, 2)); // => 3
-        System.out.println(luckySum(13)); // => 0
+        int[] d1 = {1, 3, 4};
+
+        boolean increasing = scoresIncreasing(d1);
+        System.out.println(increasing); // => true
+
+        int[] d2 = {1, 3, 2};
+        System.out.println(scoresIncreasing(d2)); // => false
+
+        int[] d3 = {1, 1, 4};
+        System.out.println(scoresIncreasing(d3)); // => true
+
+        int[] d4 = {1};
+        System.out.println(scoresIncreasing(d4)); // => true
     }
 }
