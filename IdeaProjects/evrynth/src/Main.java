00bac4432561a0b0cb1291b1ba1d2538ea37353f
nataniyasu-hash <nataniyasu@gmail.com>
2025-12-26 18:21:48 +0100

    checkBrackets revision

--- IdeaProjects/evrynth/src/Main.java
+++ IdeaProjects/evrynth/src/Main.java
@@ -1,41 +1,45 @@
-import java.util.*;
 /**
+ * ## checkBrackets()
  *
- * ## `sortWords()`
+ * Eine vollständige Klammerung bedeutet: Jeder geöffneten Klammer
+ * muss eine schließende Klammer folgen. Darüber hinaus müssen
+ * die runden Klammern korrekt verschachtelt sein. Andere Zeichen
+ * sind zu ignorieren.
  *
- * Entwickeln Sie bitte eine Methode `sortWords()`, die aus einer
- * Zeichenkette eine Liste von alphabetisch sortierten Worten erzeugt.
- * Ein Wort ist durch ein oder mehrere Leerzeichen von anderen Worten
- * getrennt.
- *
- * Treten Worte mehrfach in der Zeichenkette auf, sollen diese nur
- * einmal in der Liste alphabetisch sortierter Wörter auftreten.
- * Worte sind Case-sensitiv zu handhaben.
- *
- * __Hinweis:__
- * Denken Sie über den Einsatz einer TreeMap nach.
- * Eine TreeMap erhält das Ordnungskriterium eines Schlüssels.
- *
- * Aufrufbeispiele finden Sie in der `main()`-Methode.
+ * Schreiben Sie nun eine Methode `checkBrackets()`, die prüft, ob eine
+ * Zeichenkette den oben angegebenenen Regeln einer vollständigen
+ * Klammerung entspricht oder nicht.
  *
  */
-class Main {
-    public static List<String> sortWords(String words) {
-        Set<String> sets = new TreeSet<>();
-        List<String> list = new ArrayList();
-        if ( words.length() == 0) { list.add(""); return list; }
-        String[] array = words.split("\\s+");
-        for (int i = 0; i < array.length; i++) {
-            sets.add(array[i]);
+public class Main {
+    public static boolean checkBrackets(String zeichnen) {
+        String brackets = "";
+        for (int i = 0; i < zeichnen.length(); i++) {
+            if (zeichnen.charAt(i) == '(' || zeichnen.charAt(i) == ')')
+                brackets += zeichnen.charAt(i);
         }
-        list.addAll(sets);
-        return list;
+        if (brackets.startsWith(")")) return false;
+        if (brackets.endsWith("(")) return false;
+
+        int counter = 0;
+        for (int j = 0; j < brackets.length(); j++) {
+            if (brackets.charAt(j) == '(')
+                counter++;
+            if (brackets.charAt(j) == ')') counter--;
+            if (counter < 0) return false;
+        }
+        if (counter < 0 || counter > 0) return false;
+        return true;
     }
 
+
     public static void main(String[] args) {
-        List<String> words = sortWords("Dies ist nur ein Beispiel");
-        System.out.println(words); // => [Beispiel, Dies, ein, ist, nur]
-        System.out.println(sortWords("Abc Abc Abc"));  // => [Abc]
-        System.out.println(sortWords("abc Abc"));  // => [Abc, abc]
+
+        boolean check = checkBrackets("()");
+        System.out.println(check); // => true
+        System.out.println(checkBrackets("(()(a)(()((c))))")); // => true
+
+        System.out.println(checkBrackets("(()")); // => false
+        System.out.println(checkBrackets("a (()())a)")); // => false
     }
 }
