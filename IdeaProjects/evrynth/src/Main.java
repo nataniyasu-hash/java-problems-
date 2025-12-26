80eb030748c1bc12ad47c82c7a87e730e1fcb3d9
nataniyasu-hash <nataniyasu@gmail.com>
2025-12-26 15:30:14 +0100

    numbers revision krass

--- IdeaProjects/evrynth/src/Main.java
+++ IdeaProjects/evrynth/src/Main.java
@@ -2,48 +2,46 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * ## Palindrome in Zeichenketten bestimmen
+ * ## Dezimalzahlen in Zeichenketten bestimmen
  *
- * Palindrome sind Zeichenketten, die von vorne und hinten
- * gelesen, dasselbe Wort ergeben (z.B. "stets"). Die Groß-/
- * Kleinschreibung soll dabei ignoriert werden.
- *
- * Ein Wort ist eine zusammenhängende Zeichenkette, welches 
- * durch ein oder mehrere Leerzeichen von anderen Worten in
- * einer Zeichenkette getrennt ist.
- *
- * Entwickeln Sie nun bitte eine Methode `palindromes()`,
- * um in einem String alle Palindromwörter zu bestimmen
- * und diese Palindrome als Liste von Zeichenketten zurückzugeben.
+ * Entwickeln Sie nun bitte eine Methode `numbers()`,
+ * um in einem String alle Dezimalzahlen zu bestimmen
+ * und diese als Liste von Integer Werten zurückzugeben.
  *
  * Aufrufbeispiele finden Sie in der `main()`-Methode.
  *
+ * __Hinweise:__
+ *
+ * - Die Java-Methode `boolean Character.isDigit(char)` prüft,
+ *   ob ein Zeichen eine Ziffer ('0', '1', .. '9') ist.
+ * - Die Java-Methode `int Integer.parseInt(String)` konvertiert eine
+ *   Zeichenkette in einen `int`-Wert (z.B. "101" -> 101).
  */
 class Main {
-    public static String drehen (String wort) {
-        String gedreht = "";
-        char[] chars = wort.toLowerCase().toCharArray();
-        for (int i = chars.length - 1; i >= 0; i--) {
-            gedreht += chars[i];
-        }
-        return gedreht;
-    }
-    public static List<String> palindromes(String words) {
-        List<String> gedreht = new ArrayList<>();
-        String[] array = words.split(" ");
-        for (int i = 0; i < array.length; i++) {
-            if (array[i].toLowerCase().equals(drehen(array[i]))) {
-                gedreht.add(array[i]);
-            }
-        }
-        return gedreht;
+    public static List<Integer> numbers(String zeichnen) {
+       List<Integer> numbers = new ArrayList<>();
+       int i = 0;
+
+       while (i < zeichnen.length()) {
+           if (Character.isDigit(zeichnen.charAt(i))) {
+               int start = i;
+               while ( i < zeichnen.length() && Character.isDigit(zeichnen.charAt(i))) {
+                   i++;
+               }
+               String ekub = zeichnen.substring(start, i);
+               numbers.add(Integer.parseInt(ekub));
+           }
+           i++;
+       }
+       return numbers;
+
     }
 
     public static void main(String[] args) {
-        List<String> palindromes = palindromes("Es ist stets dasselbe Beispiel");
-        System.out.println(palindromes); // => ["stets"]
-        System.out.println(palindromes("Regallager")); // => ["Regallager"]
-        System.out.println(palindromes("no palinedromes")); // => []
-        System.out.println(palindromes("Natan ist stets weise")); // => ["Natan", "stets"]
+        List<Integer> values = numbers("This is 1 world");
+        System.out.println(values); // => [1]
+        System.out.println(numbers("no numbers")); // => []
+        System.out.println(numbers("1 12 123 1234")); // => [1, 12, 123, 1234]
+        System.out.println(numbers("ab1c23ef45gh")); // => [1, 23, 45]
     }
 }
