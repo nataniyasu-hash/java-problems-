6f6efffca91c8c70888287df9bbcfc080b71c7db
nataniyasu-hash <nataniyasu@gmail.com>
2025-12-27 19:53:08 +0100

    isprim revison

--- IdeaProjects/romms/src/Main.java
+++ IdeaProjects/romms/src/Main.java
@@ -1,67 +1,77 @@
 import java.util.*;
 
 /**
- * ## Armstrongzahlen gruppieren
+ * ## Primzahlen bestimmen
  *
- * Entwickeln Sie nun bitte die Methoden
+ * Eine Primzahl ist eine natürliche Zahl > 1, die nur durch sich
+ * selbst und 1 teilbar ist. Sie sollen nun Primzahlen
+ * generieren und tabellarisch auf der Konsole ausgeben.
  *
- * - `armstrongs()`, die eine Liste aller aufsteigend sortierter
- *   Armstrongzahlen bis zu einer oberen Schranke erzeugt,
- * - und `groupyByLength()`, die eine Liste von Integer Werten mittels einer
- *   Map nach der Anzahl ihrer Stellen gruppiert. Die Map soll das Ordnungskriterium
- *   Stellenanzahl bei einer sequentiellen Verarbeitung der Schlüssel erhalten.
+ * Entwickeln Sie hierzu bitte die folgenden Methoden:
+ *
+ * - `isPrim()` prüft, ob eine gegebene Zahl eine Primzahl ist.
+ * - `primsUntil()` erzeugt eine Liste aller aufsteigen sortierten Primzahlen
+ *   bis zu einer oberen Schranke.
+ * - `columnize()` erzeugt aus einer Liste eine Zeichenkette in dem jedes Element
+ *   mit einem Tabulator `\t` getrennt wird. Jeder n.te Tabulator wird jedoch
+ *   durch ein `\n` ersetzt (solche Zeichenketten erscheinen tabellarisch
+ *   auf der Konsole).
  *
  * Aufrufbeispiele finden Sie in der `main()`-Methode.
  *
  * __Hinweise:__
  *
- * - Sie haben bereits einmal eine Lösung entwickelt, die prüft, ob eine Zahl eine Armstrongzahl ist.
- * - Beachten Sie die Wahl ihrer Map-Implementierung.
+ * - https://de.wikipedia.org/wiki/Primzahl
+ * - Kennen Sie noch die String Methode `trim()`?
  *
  */
 class Main {
-    public static boolean armstrong(int a ) {
-        int zahl = a;
-        int length = String.valueOf(a).length();
-        int sum = 0;
-        while (zahl > 0) {
-            int ziffer = zahl % 10;
-            double potenz = Math.pow(ziffer, length);
-            sum += potenz;
-            zahl = zahl / 10;
-
+    public static boolean isPrim(int a ) {
+        int count = 2;
+        if (a <= 1) return false;
+        for (int i = 2; i < a; i++) {
+            if (a % i == 0) count++;
         }
-        if (sum == a ) return true;
-        return false;
+        if (count > 2) return false;
+        return true;
     }
-    public static List<Integer> armstrongs(int a ) {
-        List<Integer> armstrong = new ArrayList<>();
-        Set<Integer>  sets = new TreeSet<>();
-        for (int i = 0; i <= a; i ++) {
-            if (armstrong(i)) sets.add(i);
+    public static List<Integer> primsUntil(int a ) {
+        List<Integer> prims = new ArrayList<>();
+        Set<Integer> sets = new TreeSet<>();
+        for (int i = 0; i < a; i++) {
+            if(isPrim(i))
+                sets.add(i);
         }
-        armstrong.addAll(sets);
-
-        return armstrong;
-    }
-    public static Map <Integer, List<Integer>> groupByLength(List<Integer> list) {
-        Map<Integer, List<Integer>> grouped = new TreeMap<>();
-        for (int i = 0; i < list.size(); i++) {
-            grouped.put(String.valueOf(list.get(i)).length(), new ArrayList<>());
-        }
-        for (int i = 0; i < list.size(); i++) {
-            grouped.get(String.valueOf(list.get(i)).length()).add(list.get(i));
-        }
-        return grouped;
+        prims.addAll(sets);
+        return prims;
     }
 
+    public static String columnize(List<Integer> prims, int a) {
+        String ordered = "";
+
+        for (int i = 0; i < prims.size(); i++) {
+            if( (i + 1) % a != 0) {
+                ordered += prims.get(i) + "\t";
+            } else {
+                ordered += prims.get(i) +"\n";
+            }
+        }
+
+        return ordered;
+    }
     public static void main(String[] args) {
-        List<Integer> lance = armstrongs(500);
-        System.out.println(lance);
-        // [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 153, 370, 371, 407]
+        boolean prim = isPrim(7);
+        System.out.println(prim); // => true
 
-        Map<Integer, List<Integer>> grouped = groupByLength(lance);
-        System.out.println(grouped);
-        // {1=[0, 1, 2, 3, 4, 5, 6, 7, 8, 9], 3=[153, 370, 371, 407]}
+        List<Integer> prims = primsUntil(20);
+        System.out.println(prims); // => [2, 3, 5, 7, 11, 13, 17, 19]
+
+        String output = columnize(prims, 3);
+        System.out.println(output);
+        // 2   3   5
+        // 7   11  13
+        // 17  19
+
+        // Entspricht der Zeichenkette: "2\t3\t5\n7\t11\t13\n17\t19"
     }
 }
