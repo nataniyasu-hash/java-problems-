53a014ae7fb46425c6f825f06dc1b33d54f1fd46
nataniyasu-hash <nataniyasu@gmail.com>
2025-12-27 16:55:20 +0100

    armstrong sorted and grouped

--- IdeaProjects/romms/src/Main.java
+++ IdeaProjects/romms/src/Main.java
@@ -1,38 +1,67 @@
+import java.util.*;
+
 /**
- * ## Räume
+ * ## Armstrongzahlen gruppieren
  *
- * Sie sollen nun eine Klasse anlegen, mit denen man
- * Räume verwalten kann, die dem Benennungsschema
- * der TH Lübeck entsprechen.
+ * Entwickeln Sie nun bitte die Methoden
  *
- * Ein Raum liegt
+ * - `armstrongs()`, die eine Liste aller aufsteigend sortierter
+ *   Armstrongzahlen bis zu einer oberen Schranke erzeugt,
+ * - und `groupyByLength()`, die eine Liste von Integer Werten mittels einer
+ *   Map nach der Anzahl ihrer Stellen gruppiert. Die Map soll das Ordnungskriterium
+ *   Stellenanzahl bei einer sequentiellen Verarbeitung der Schlüssel erhalten.
  *
- * - in einem Gebäude,
- * - auf einer Etage
- * - und hat eine Raumnummer (< 100).
- *
- * Wie Räume angelegt und ausgegeben werden können,
- * lässt sich den Beispielen in der `main()`-Methode entnehmen.
- *
- * Ergänzend sollen Räume noch gem. Java Konventionen
- * inhaltlich verglichen und geklont werden können.
- *
+ * Aufrufbeispiele finden Sie in der `main()`-Methode.
  *
  * __Hinweise:__
  *
- * - Sehen Sie sich die relevanten Punkte in Unit 03 noch einmal an.
- * - Sehen Sie sich nochmal `String.format()` in Unit 01 an.
+ * - Sie haben bereits einmal eine Lösung entwickelt, die prüft, ob eine Zahl eine Armstrongzahl ist.
+ * - Beachten Sie die Wahl ihrer Map-Implementierung.
  *
  */
 class Main {
+    public static boolean armstrong(int a ) {
+        int zahl = a;
+        int length = String.valueOf(a).length();
+        int sum = 0;
+        while (zahl > 0) {
+            int ziffer = zahl % 10;
+            double potenz = Math.pow(ziffer, length);
+            sum += potenz;
+            zahl = zahl / 10;
+
+        }
+        if (sum == a ) return true;
+        return false;
+    }
+    public static List<Integer> armstrongs(int a ) {
+        List<Integer> armstrong = new ArrayList<>();
+        Set<Integer>  sets = new TreeSet<>();
+        for (int i = 0; i <= a; i ++) {
+            if (armstrong(i)) sets.add(i);
+        }
+        armstrong.addAll(sets);
+
+        return armstrong;
+    }
+    public static Map <Integer, List<Integer>> groupByLength(List<Integer> list) {
+        Map<Integer, List<Integer>> grouped = new TreeMap<>();
+        for (int i = 0; i < list.size(); i++) {
+            grouped.put(String.valueOf(list.get(i)).length(), new ArrayList<>());
+        }
+        for (int i = 0; i < list.size(); i++) {
+            grouped.get(String.valueOf(list.get(i)).length()).add(list.get(i));
+        }
+        return grouped;
+    }
 
     public static void main(String[] args) {
-        Room office = new Room(17, 0, 10);
-        Room lecture = new Room(2, 0, 10);
-        Room lab = new Room(18, 1, 1);
+        List<Integer> lance = armstrongs(500);
+        System.out.println(lance);
+        // [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 153, 370, 371, 407]
 
-        System.out.println(office);  // => "17-0.10"
-        System.out.println(lecture); // => " 2-0.10"
-        System.out.println(lab);     // => "18-1.01"
+        Map<Integer, List<Integer>> grouped = groupByLength(lance);
+        System.out.println(grouped);
+        // {1=[0, 1, 2, 3, 4, 5, 6, 7, 8, 9], 3=[153, 370, 371, 407]}
     }
 }
