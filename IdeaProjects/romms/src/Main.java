a1bfd71b775fdd979130c95fa279620d1a555ea3
nataniyasu-hash <nataniyasu@gmail.com>
2026-01-01 18:11:52 +0100

    sortwords

--- IdeaProjects/romms/src/Main.java
+++ IdeaProjects/romms/src/Main.java
@@ -1,49 +1,44 @@
-import java.util.ArrayList;
 import java.util.List;
-
+import java.util.ArrayList;
+import java.util.Set;
+import java.util.TreeSet;
 /**
- * ## Blöcke in Zeichenketten bestimmen.
  *
- * Entwickeln Sie nun bitte eine Methode `blocks()`,
- * um in einem String alle Blöcke gleicher aufeinander folgender Zeichen
- * zu bestimmen.
+ * ## `sortWords()`
+ *
+ * Entwickeln Sie bitte eine Methode `sortWords()`, die aus einer
+ * Zeichenkette eine Liste von alphabetisch sortierten Worten erzeugt.
+ * Ein Wort ist durch ein oder mehrere Leerzeichen von anderen Worten
+ * getrennt.
+ *
+ * Treten Worte mehrfach in der Zeichenkette auf, sollen diese nur
+ * einmal in der Liste alphabetisch sortierter Wörter auftreten.
+ * Worte sind Case-sensitiv zu handhaben.
+ *
+ * __Hinweis:__
+ * Denken Sie über den Einsatz einer TreeMap nach.
+ * Eine TreeMap erhält das Ordnungskriterium eines Schlüssels.
  *
  * Aufrufbeispiele finden Sie in der `main()`-Methode.
  *
  */
 class Main {
-    public static List<String> blocks(String words) {
-        List<String> blocks = new ArrayList<>();
-        int count = 1;
-        if (words.length() == 0) return blocks;
-        char temp = words.charAt(0);
-        for (int i = 1; i < words.length(); i++) {
-            if (temp == words.charAt(i)) {
-                count++;
-                temp = words.charAt(i);
-            } else {
-                if (count > 1) {
-                    String ekub = String.valueOf(temp).repeat(count);
-                    blocks.add(ekub);
-                    count = 1;
-                }
-                temp = words.charAt(i);
+    public static List<String> sortWords(String words) {
+        Set<String> sorted = new TreeSet<>();
+        List<String> ordered = new ArrayList<>();
+        String[] word = words.split(" ");
 
-            }
+        for (int i = 0; i < word.length; i++) {
+            sorted.add(word[i]);
         }
-        if (count > 1) {
-            String ekub = String.valueOf(temp).repeat(count);
-            blocks.add(ekub);
-        }
-        return blocks;
+        ordered.addAll(sorted);
+        return ordered;
     }
 
     public static void main(String[] args) {
-        List<String> blocks = blocks("Hello faaantastic world");
-        System.out.println(blocks); // => ["ll", "aaa"]
-        System.out.println(blocks("aaabccdeeeefaaa")); // => ["aaa", "cc", "eeee", "aaa"]
-        System.out.println(blocks("This is an example")); // => []
-        System.out.println(blocks("Another  example ...")); // => ["  ", "..."]
-        System.out.println(blocks("")); // => []
+        List<String> words = sortWords("Dies ist nur ein Beispiel");
+        System.out.println(words); // => [Beispiel, Dies, ein, ist, nur]
+        System.out.println(sortWords("Abc Abc Abc"));  // => [Abc]
+        System.out.println(sortWords("abc Abc"));  // => [Abc, abc]
     }
 }
