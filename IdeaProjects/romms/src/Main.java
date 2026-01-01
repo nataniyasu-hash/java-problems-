5377da550cada39a89fbd7d08e2d07e871d5ef00
nataniyasu-hash <nataniyasu@gmail.com>
2026-01-01 18:06:45 +0100

    blocks revision

--- IdeaProjects/romms/src/Main.java
+++ IdeaProjects/romms/src/Main.java
@@ -1,58 +1,49 @@
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Arrays;
 
 /**
+ * ## Blöcke in Zeichenketten bestimmen.
  *
- * ## `zeroMax()`
+ * Entwickeln Sie nun bitte eine Methode `blocks()`,
+ * um in einem String alle Blöcke gleicher aufeinander folgender Zeichen
+ * zu bestimmen.
  *
- * Entwickeln Sie nun bitte eine Methode `zeroMax()`, die ein Integer Array aus
- * einer übergebenen Liste von Integer Werten erzeugt, bei der jeder Nullwert
- * (0, also nicht null!!!)
- * in der Liste durch den größten positiven Wert rechts von der Null ersetzt wird.
- * Wenn es rechts von der Null keinen größten positiven Wert gibt, wird die Null
- * als Null belassen.
- *
- * Aufruf-Beispiele finden Sie in der `main()`-Methode.
- *
- * Hinweis: Wenn Ihnen der Datentyp List (noch) nichts sagt, fragen Sie KIRA
- * was das für ein Datentyp ist und wie man mit diesem umgeht.
+ * Aufrufbeispiele finden Sie in der `main()`-Methode.
  *
  */
-public class Main {
-    public static int[] zeroMax(List<Integer> list) {
-        int[] zeroMax = new int[list.size()];
-        for (int i = 0; i <list.size(); i++) {
-            if(list.get(i) != 0) {
-                zeroMax[i] = list.get(i);
-            }
-            if (list.get(i) == 0) {
-                int max = 0;
-                int temp= 0;
-
-                for (int j = i + 1; j < list.size(); j++) {
-                    if (list.get(j) != 0) {
-                         temp = list.get(j);
-                    }
-                    if (temp > max ) {
-                        max = temp;
-                    }
+class Main {
+    public static List<String> blocks(String words) {
+        List<String> blocks = new ArrayList<>();
+        int count = 1;
+        if (words.length() == 0) return blocks;
+        char temp = words.charAt(0);
+        for (int i = 1; i < words.length(); i++) {
+            if (temp == words.charAt(i)) {
+                count++;
+                temp = words.charAt(i);
+            } else {
+                if (count > 1) {
+                    String ekub = String.valueOf(temp).repeat(count);
+                    blocks.add(ekub);
+                    count = 1;
                 }
-                zeroMax[i] = max;
+                temp = words.charAt(i);
+
             }
         }
-        return zeroMax;
+        if (count > 1) {
+            String ekub = String.valueOf(temp).repeat(count);
+            blocks.add(ekub);
+        }
+        return blocks;
     }
 
     public static void main(String[] args) {
-        List<Integer> list = Arrays.asList(0, 5, 0, 3);
-        int[] result = zeroMax(list);
-        System.out.println(Arrays.toString(result));        // => [5, 5, 3, 3]
-
-        list = Arrays.asList(0, 4, 0, 3);
-        System.out.println(Arrays.toString(zeroMax(list))); // => [4, 4, 3, 3]
-
-        list = Arrays.asList(0, 1, 0);
-        System.out.println(Arrays.toString(zeroMax(list)));  // => [1, 1, 0]
+        List<String> blocks = blocks("Hello faaantastic world");
+        System.out.println(blocks); // => ["ll", "aaa"]
+        System.out.println(blocks("aaabccdeeeefaaa")); // => ["aaa", "cc", "eeee", "aaa"]
+        System.out.println(blocks("This is an example")); // => []
+        System.out.println(blocks("Another  example ...")); // => ["  ", "..."]
+        System.out.println(blocks("")); // => []
     }
 }
