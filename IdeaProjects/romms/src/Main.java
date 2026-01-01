b0286140e4ab914a2bfb99a5179c27de77e710de
nataniyasu-hash <nataniyasu@gmail.com>
2026-01-01 18:48:06 +0100

    wordsAppend

--- IdeaProjects/romms/src/Main.java
+++ IdeaProjects/romms/src/Main.java
@@ -1,44 +1,46 @@
-import java.util.List;
-import java.util.ArrayList;
-import java.util.Set;
-import java.util.TreeSet;
+import java.util.*;
+
 /**
+ * ## Listen von Zeichenketten mittels `wordAppend()`verarbeiten
  *
- * ## `sortWords()`
+ * Entwickeln Sie nun bitte eine Methode `wordAppend()`.
+ * Diese soll eine Liste von Strings durchlaufen, um einen Ausgabestring
+ * zu erzeugen. Gehen Sie dabei wie folgt vor:
  *
- * Entwickeln Sie bitte eine Methode `sortWords()`, die aus einer
- * Zeichenkette eine Liste von alphabetisch sortierten Worten erzeugt.
- * Ein Wort ist durch ein oder mehrere Leerzeichen von anderen Worten
- * getrennt.
- *
- * Treten Worte mehrfach in der Zeichenkette auf, sollen diese nur
- * einmal in der Liste alphabetisch sortierter Wörter auftreten.
- * Worte sind Case-sensitiv zu handhaben.
- *
- * __Hinweis:__
- * Denken Sie über den Einsatz einer TreeMap nach.
- * Eine TreeMap erhält das Ordnungskriterium eines Schlüssels.
+ * Immer wenn ein String zum 2., 4., 6., usw. mal in der Liste auftaucht,
+ * soll der String an den Ausgabestring gehängt werden. Wenn kein String doppelt
+ * vorkommt, soll die leere Zeichenkette zurückgegeben werden.
  *
  * Aufrufbeispiele finden Sie in der `main()`-Methode.
  *
+ * __Hinweis:__
+ *
+ * - Denken Sie über den Einsatz einer geeigneten Datenstruktur
+ *   (`Collection`) nach.
+ *
  */
-class Main {
-    public static List<String> sortWords(String words) {
-        Set<String> sorted = new TreeSet<>();
-        List<String> ordered = new ArrayList<>();
-        String[] word = words.split(" ");
-
-        for (int i = 0; i < word.length; i++) {
-            sorted.add(word[i]);
+public class Main {
+    public static String wordAppend(List<String> list) {
+        String append = "";
+        int count = 1;
+        Map<String, Integer> counter = new LinkedHashMap<>();
+        for (int i = 0; i < list.size(); i++) {
+            counter.put(list.get(i), counter.getOrDefault(list.get(i), 0) + 1);
+            if(counter.get(list.get(i)) % 2 == 0) {
+                append += list.get(i);
+            }
         }
-        ordered.addAll(sorted);
-        return ordered;
+        return append;
     }
 
     public static void main(String[] args) {
-        List<String> words = sortWords("Dies ist nur ein Beispiel");
-        System.out.println(words); // => [Beispiel, Dies, ein, ist, nur]
-        System.out.println(sortWords("Abc Abc Abc"));  // => [Abc]
-        System.out.println(sortWords("abc Abc"));  // => [Abc, abc]
+        List<String> example = Arrays.asList("a", "b", "a");
+        String result = wordAppend(example);
+        System.out.println(result); // -> "a"
+        System.out.println(wordAppend(
+                Arrays.asList("a", "b", "a", "c", "a", "d", "a")
+        )); // -> "aa"
+        System.out.println(wordAppend(Arrays.asList("a", "", "a"))); // -> "a"
     }
+
 }
