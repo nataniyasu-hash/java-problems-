870b0dbcd10d31181f124e61d79e1a42f5865efd
nataniyasu-hash <nataniyasu@gmail.com>
2026-01-02 13:32:54 +0100

    wordappend revision

--- IdeaProjects/romms/src/Main.java
+++ IdeaProjects/romms/src/Main.java
@@ -1,59 +1,51 @@
-import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Arrays;
+import java.util.Map;
 
 /**
- * ## Liste von Blöcken aus Zeichenketten extrahieren
+ * ## Listen von Zeichenketten mittels `wordAppend()`verarbeiten
  *
- * Unter einem Block verstehen wir eine Folge gleicher Zeichen, z.B. “aaaa”.
- * Blöcke mit mehr als einem Zeichen, können kompakt dargestellt werden,
- * indem nur das Zeichen und die Wiederholung angegeben wird
- * (die Kompaktschreibweise für “aaaa” wäre “a4”).
- * Blöcke der Länge 1 werden nicht in Kompaktschreibweise notiert
- * (“a” wird also nie zu “a1”).
+ * Entwickeln Sie nun bitte eine Methode `wordAppend()`.
+ * Diese soll eine Liste von Strings durchlaufen, um einen Ausgabestring
+ * zu erzeugen. Gehen Sie dabei wie folgt vor:
  *
- * Entwickeln Sie nun bitte eine Methode `compacts()`,
- * die aus einer beliebigen Zeichenkette eine Liste von Blöcken in
- * Kompaktschreibweise erzeugt.
+ * Immer wenn ein String zum 2., 4., 6., usw. mal in der Liste auftaucht,
+ * soll der String an den Ausgabestring gehängt werden. Wenn kein String doppelt
+ * vorkommt, soll die leere Zeichenkette zurückgegeben werden.
  *
  * Aufrufbeispiele finden Sie in der `main()`-Methode.
  *
+ * __Hinweis:__
+ *
+ * - Denken Sie über den Einsatz einer geeigneten Datenstruktur
+ *   (`Collection`) nach.
+ *
  */
-class Main {
-    public static List<String> compacts(String word) {
-        List<String> list = new ArrayList<>();
-        if (word.length() == 0) return list;
-        int count = 1;
-        char temp = word.charAt(0);
+public class Main {
+    public static String wordAppend(List<String> list) {
+        String append = "";
+        Map<String, Integer> counter = new HashMap<>();
 
-        for (int i = 1; i < word.length(); i++) {
-            if (temp == word.charAt(i)) {
-                count++;
-                temp = word.charAt(i);
-            } else {
-                if (count > 1) {
-                    list.add(String.valueOf(temp) + count);
-
-                }
-                if (count == 1) {
-                list.add(String.valueOf(temp));
-                }
-                temp = word.charAt(i);
-                count = 1;
+        for (int i = 0; i < list.size(); i++) {
+            counter.put(list.get(i), counter.getOrDefault(list.get(i), 0) + 1);
+            if(counter.get(list.get(i)) % 2 == 0) {
+                append += list.get(i);
             }
         }
-        if (count > 1) {
-        list.add(String.valueOf(temp) + count);
-        } else {
-            list.add(String.valueOf(temp));
-        }
-        return list;
+
+
+        return append;
     }
+
     public static void main(String[] args) {
-        List<String> blocks = compacts("Hello");
-        System.out.println(blocks); // => ["H", "e", "l2", "o"]
-        System.out.println(compacts("Oooorder")); // => ["O", "o3", "r", "d", "e", "r"]
-        System.out.println(compacts("C3PO")); // => ["C", "3", "P", "O"]
-        System.out.println(compacts("...")); // => [".3"]
-        System.out.println(compacts("")); // => []
+        List<String> example = Arrays.asList("a", "b", "a");
+        String result = wordAppend(example);
+        System.out.println(result); // -> "a"
+        System.out.println(wordAppend(
+                Arrays.asList("a", "b", "a", "c", "a", "d", "a")
+        )); // -> "aa"
+        System.out.println(wordAppend(Arrays.asList("a", "", "a"))); // -> "a"
     }
+
 }
