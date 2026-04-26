b9d3cee11fab95e0010681e42df9bb11e302fd39
nataniyasu-hash <nataniyasu@gmail.com>
2026-04-26 21:54:47 +0200

    finished

--- src/Lists/MapFilter.hs
+++ src/Lists/MapFilter.hs
@@ -9,4 +9,4 @@ euler1 :: Int
 euler1 = sum (filter (\x -> x `mod` 3 == 0 || x `mod` 5 == 0) [0..999])
 
 finde :: (a -> Bool) -> [a] -> a
-finde p xs = head  (filter p xs)
+finde p = head . filter p
